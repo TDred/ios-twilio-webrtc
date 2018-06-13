1287f2d3ec53120e08f041ba8342d47aee2f5dab
Timofey Butorin <tbutorin@appgear.ca>
2018-06-14 00:36:42 +0300

    Text fields appearance and input behaviour

--- WEBRtcTest/Views/CRCredentialsView.h
+++ WEBRtcTest/Views/CRCredentialsView.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CRCredentialsView : UIView
+@interface CRCredentialsView : UIView <UITextFieldDelegate>
 
 @end
--- WEBRtcTest/Views/CRCredentialsView.m
+++ WEBRtcTest/Views/CRCredentialsView.m
@@ -45,9 +45,13 @@
     [self addSubview:self.btnConnect];
     
     self.txtName = [[UITextField alloc] init];
+    self.txtName.delegate = self;
+    self.txtName.tag = 1;
     [self addSubview:self.txtName];
     
     self.txtRoom = [[UITextField alloc] init];
+    self.txtRoom.delegate = self;
+    self.txtRoom.tag = 2;
     [self addSubview:self.txtRoom];
     
     UIView *superview = self;
@@ -90,6 +94,8 @@
     return self;
 }
 
+#pragma mark - UITextFieldDelegate implementation
+
 - (void)layoutSubviews
 {
     [super layoutSubviews];
@@ -101,6 +107,31 @@
     
     CGRect roomFrame = self.txtRoom.frame;
     [self.txtRoom setDefaultTheme:RGB(0xD9D3D2) withBottomBorderColor:RGB(0xCC3728) withFrame:roomFrame];
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    textField.backgroundColor = RGBA(0xD9D3D264);
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    textField.backgroundColor = RGB(0xD9D3D2);
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSInteger nextTag = textField.tag + 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [textField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [textField resignFirstResponder];
+    }
+    return NO;
 }
 
 @end
